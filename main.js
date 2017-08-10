fbf2719acacf1f189d98404c05db8eee3e45585d
lymanjohnson <lyman.johnson@gmail.com>
2017-08-09 20:21:00 -0400

    Works, adding better behavior

--- main.js
+++ main.js
@@ -1,25 +1,70 @@
 
-buttonsElements = document.getElementsByClassName("calc-button");
+buttonsElements = document.getElementsByClassName("calcButton");
+wrapper = document.getElementById("wrapper");
 
-field = "";
+console.log(wrapper);
 
-buttons = {};
+let buttons = {};
+let memoryLog = [];
+let newEntry = true;
 
 for (i=0;i<buttonsElements.length;i++){
   let thisButton = buttonsElements[i];
-  console.log(thisButton);
-  if(thisButton.classList.contains("number")){
+  if (thisButton.classList.contains("normal")){
     thisButton.value = thisButton.innerHTML;
-  }
+    thisButton.type  = "normal"}
+  else {thisButton.value = thisButton.id;
+        thisButton.type = "special"}
   thisButton.addEventListener("click",clickFunction);
   buttons[thisButton.id] = thisButton;
 }
 
-console.log(buttons);
-console.log(buttons.one);
+//special exceptions:
+buttons.power.value = "^";
+buttons.modulo.value = "%";
+buttons.times.value = "*";
+buttons.divide.value = "/";
+
 
 function clickFunction() {
-  field += this.value;
-  buttons.displayArea
-  console.log(field);
+    if (this.type == "normal") {
+      if(newEntry == true){
+        if(this.type == "number") {buttons.displayArea.textContent="";}
+        newEntry = false;
+      }
+      buttons.displayArea.textContent += this.value;
+      buttons.clear.textContent = "C";}
+    else if (this.value == "clear") {clickClear();}
+    else if (this.value == "memory") {clickMemory();}
+    else if (this.value == "equalsign") {clickEquals();}
+}
+
+function clickMemory() {
+  console.log(memoryLog);
+  buttons.displayArea.textContent = memoryLog.pop();
+}
+
+function clickClear() {
+  // console.log(buttons.displayArea.textContent);
+  if (buttons.displayArea.textContent !== ""){
+    buttons.displayArea.textContent = "";
+    buttons.clear.textContent = "CE";
+  }
+  else {
+    wrapper.classList.remove("flipped");
+    wrapper.classList.add("flip");
+    setTimeout(function () {
+      wrapper.classList.remove("flip");
+      wrapper.classList.add("flipped");
+    }, 500);
+    memoryLog = [];
+  };
+}
+
+function clickEquals(){
+  newEntry = true;
+  console.log(buttons.displayArea.textContent);
+  let answer = buttons.displayArea.textContent;
+  memoryLog.push(answer);
+  buttons.displayArea.textContent = eval(answer);
 }
