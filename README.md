7bb37dbc0cb6a6aba72954ea5ddbe3facae3e2f7
lymanjohnson <lyman.johnson@gmail.com>
2017-08-10 15:29:45 -0400

    Finished

---
+++ README.md
@@ -0,0 +1,10 @@
+Multi-Function Calculator With Robust Error Proofing
+
+- Logic loops prevent user from entering input that would throw most parsing errors
+- Buttons flash red if pushing it would have created an error
+- Memory log saves all entries (not answers, but the entry that yielded the answer)
+- Given sequential identical entries, only the most recent one will be stored in the memory
+- Clear button displays "C" and clears line if there is an entry in the display area.
+- If the display is empty, the clear button will change to "CM" and will clear the memory if pressed.
+- Clearing the memory triggers a gray "flash" effect.
+- Semi-responsive design fits calculator to almost any window size
--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-// //
-// let math = require('mathjs');
-// let limitedEval = math.eval;
-//
-// math.import({
-//   'import':     function () { throw new Error('Function import is disabled') },
-//   'createUnit': function () { throw new Error('Function createUnit is disabled') },
-//   // 'eval':       function () { throw new Error('Function eval is disabled') },
-//   'parse':      function () { throw new Error('Function parse is disabled') },
-//   'simplify':   function () { throw new Error('Function simplify is disabled') },
-//   'derivative': function () { throw new Error('Function derivative is disabled') }
-// }, {override: true});
 
 
 calculator = document.getElementById("calculator");
@@ -31,22 +19,6 @@ for (i=0;i<calculator.children.length;i++){
   buttons[thisButton.id] = thisButton;
 }
 
-// for (i=0;i<buttonsElements.length;i++){
-//   console.log(i);
-//   let thisButton = buttonsElements[i];
-//   thisButton.type  = thisButton.classList[0];
-//   thisButton.addEventListener("click",clickFunction);
-//   console.log(thisButton.classList.contains("special"));
-//   if (thisButton.classList.contains("special")==false){
-//     thisButton.value = thisButton.innerHTML;
-//   }
-//   else {
-//     thisButton.value = thisButton.id;
-//   }
-//
-//   buttons[thisButton.id] = thisButton;
-// }
-
 //special exceptions:
 buttons.power.value = "^";
 buttons.modulo.value = "%";
@@ -56,23 +28,20 @@ buttons.backspace.value = "backspace";
 
 
 function clicker() {
-    console.log(this);
     if      (this.type == "number")   {clickNumber(this);}
-    else if (this.type == "function") {clickFunction(this)}
-    else if (this.value == "CM")      {clickClear();}
-    else if (this.value == "MEM")     {clickMemory();}
-    else if (this.value == "=")       {clickEquals();}
-    else if (this.value == "backspace"){
-      clickBackspace();
-    }
+    else if (this.type == "function") {clickFunction(this);}
+    else if (this.value == "CM")      {clickClear(this);}
+    else if (this.value == "MEM")     {clickMemory(this);}
+    else if (this.value == "=")       {clickEquals(this);}
+    else if (this.value == "backspace") {clickBackspace(this);}
 }
 
 
 function clickNumber(thisButton) {
-  if ((lastPress.type=="function")&&(thisButton.value==")")) {}
-  else if (thisButton.value==")" && closeParenCount>=openParenCount){}
-  else if (lastPress.value==")" && thisButton.type =="number" && thisButton.value!=")" && !newEntry){}
-  else if (lastPress.value=="(" && thisButton.value==")"){}
+  if ((lastPress.type=="function")&&(thisButton.value==")")) {womp(thisButton)}
+  else if (thisButton.value==")" && closeParenCount>=openParenCount){womp(thisButton)}
+  else if (lastPress.value==")" && thisButton.type =="number" && thisButton.value!=")" && !newEntry){womp(thisButton)}
+  else if (lastPress.value=="(" && thisButton.value==")"){womp(thisButton)}
   else if (newEntry) {
     buttons.displayArea.textContent = thisButton.value;
     newEntry = false;
@@ -82,8 +51,7 @@ function clickNumber(thisButton) {
     if(thisButton.value == ")"){closeParenCount+=1}
   }
   else {
-    // if (thisButton.value=="(" && ((lastPress.type != "function" || lastPress.value != "(") && buttons.displayArea.textContent != "")){}
-    if (thisButton.value=="(" && lastPress.value != "(" && lastPress.type != "function" ) {}
+    if (thisButton.value=="(" && lastPress.value != "(" && lastPress.type != "function" ) {womp(thisButton)}
     else{
       buttons.displayArea.textContent += thisButton.value;
       lastPress = thisButton;
@@ -96,12 +64,11 @@ function clickNumber(thisButton) {
 }
 
 function clickFunction(thisButton) {
-    console.log(lastPress);
-    if (buttons.displayArea.textContent == "" && thisButton.value != "-" && thisButton.value != "+") {}
-    else if ((buttons.displayArea.textContent == "+" || buttons.displayArea.textContent == "-") && thisButton.value != "-" && thisButton.value != "+"){}
-    else if ((lastPress.value=="(")&&(thisButton.value=="*" || thisButton.value=="/" || lastPress.value==".")){}
+    if (buttons.displayArea.textContent == "" && thisButton.value != "-" && thisButton.value != "+") {womp(thisButton)}
+    else if ((buttons.displayArea.textContent == "+" || buttons.displayArea.textContent == "-") && thisButton.value != "-" && thisButton.value != "+"){womp(thisButton)}
+    else if ((lastPress.value=="(")&&(thisButton.value=="*" || thisButton.value=="/" || lastPress.value==".")){womp(thisButton)}
 
-    else if (lastPress.value == "."){}
+    else if (lastPress.value == "."){womp(thisButton)}
 
     else if (lastPress.type == "function"){
       buttons.displayArea.textContent = buttons.displayArea.textContent.substr(0, buttons.displayArea.textContent.length - 1)+thisButton.value;
@@ -116,8 +83,8 @@ function clickFunction(thisButton) {
     }
 }
 
-function clickMemory() {
-  console.log(memoryLog);
+function clickMemory(thisButton) {
+  //console.log(memoryLog);
   buttons.displayArea.textContent = memoryLog.pop();
   openParenCount = 0;
   closeParenCount = 0;
@@ -125,7 +92,7 @@ function clickMemory() {
   lastPress = "";
 }
 
-function clickClear() {
+function clickClear(thisButton) {
   if (buttons.displayArea.textContent !== ""){
     buttons.displayArea.textContent = "";
     buttons.clear.textContent = "CM";
@@ -149,36 +116,34 @@ function clickClear() {
   };
 }
 
-function clickEquals(){
+function clickEquals(thisButton){
   // try{
     let answer = buttons.displayArea.textContent;
     answer = answer.replace("^","**");
 
-    if(lastPress.value == "(" || lastPress.type=="function" || lastPress.value == "."){}
+    if(lastPress.value == "(" || lastPress.type=="function" || lastPress.value == "."){womp(thisButton)}
 
-    else if (openParenCount != closeParenCount){}
+    else if (openParenCount != closeParenCount){womp(thisButton)}
 
     else{
       newEntry = true;
       if (memoryLog[memoryLog.length-1] != answer){
-        console.log(memoryLog[memoryLog.length-1],answer);
         memoryLog.push(answer);}
       buttons.displayArea.textContent = eval(answer);
     }
 
 }
 
-function clickBackspace(){
-  let display = buttons.displayArea.textContent;
-  console.log(display);
-  if (display = "") {}
+function clickBackspace(thisButton){
+  if (buttons.displayArea.textContent.length==1){buttons.clear.textContent = "CM"}
+  if (buttons.displayArea.textContent == "") {
+    buttons.clear.textContent = "CM"
+    womp(thisButton)}
   else {
     if (buttons.displayArea.textContent.slice(-1) == ")") {closeParenCount--}
     if (buttons.displayArea.textContent.slice(-1) == "(") {openParenCount--}
     buttons.displayArea.textContent = buttons.displayArea.textContent.substr(0, buttons.displayArea.textContent.length - 1);
-    console.log(display);
     let lastChar = buttons.displayArea.textContent.slice(-1);
-    console.log("lastChar",lastChar);
     if      (lastChar == "1") {lastPress   =  buttons.one}
     else if (lastChar == "2")  {lastPress  =  buttons.two}
     else if (lastChar == "3")  {lastPress  =  buttons.three}
@@ -198,8 +163,16 @@ function clickBackspace(){
     else if (lastChar == "-")  {lastPress  =  buttons.minus}
     else if (lastChar == "/")  {lastPress  =  buttons.divide}
     else if (lastChar == "*")  {lastPress  =  buttons.times}
-    console.log("lastPress.value",lastPress.value);
-    console.log("lastPress.type",lastPress.type);
 
   }
+}
+
+function womp(thisButton) {
+  thisButton.classList.remove("womped");
+  thisButton.classList.add("womp");
+  setTimeout(function () {
+    thisButton.classList.remove("womp");
+    thisButton.classList.add("womped");
+  }, 500);
+
 }
